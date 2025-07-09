57e018e8e49d3c9b1444eaf738af2ba0c6307406
January Toledo <23102476@usc.edu.ph>
2025-07-09 15:16:41 +0800

    welcome activity redirecting to home

--- app/src/main/java/ph/edu/usc/petalpress/WelcomeActivity.java
+++ app/src/main/java/ph/edu/usc/petalpress/WelcomeActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-//redirect to home
+
 public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
