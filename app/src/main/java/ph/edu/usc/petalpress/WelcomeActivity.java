242d2b7480078f037e9a3ac47314476eec62ad34
Sharly Pia Rose Barago <155881364+sharly-barago@users.noreply.github.com>
2025-07-10 08:32:59 +0800

    homepage update 1

--- app/src/main/java/ph/edu/usc/petalpress/WelcomeActivity.java
+++ app/src/main/java/ph/edu/usc/petalpress/WelcomeActivity.java
@@ -3,6 +3,8 @@ package ph.edu.usc.petalpress;
 import android.os.Bundle;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -11,6 +13,16 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        TextView startBtn = findViewById(R.id.startWritingButton);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this, Homepage.class);
+                startActivity(intent);
+                finish(); // Optional: so user can't go back to welcome
+            }
+        });
 
     }
 }
